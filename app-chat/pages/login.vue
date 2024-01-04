0bf879383c9799d1aa446f7a6f531e019b5333a3
Kacper Nalepa <kacper.nlp@wp.pl>
2024-01-04 22:31:06 +0100

    Create view for login and register page

---
+++ app-chat/pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-card shadow="always">
+      <el-form v-model="loginForm">
+        <el-form-item label="Login">
+          <el-input v-model="loginForm.login" />
+        </el-form-item>
+        <el-form-item label="Password">
+          <el-input v-model="loginForm.password" type="password" />
+        </el-form-item>
+        <el-button type="primary" @click.prevent="handleSubmitLogin">
+          Login
+        </el-button>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const loginForm = ref({
+  login: "",
+  password: "",
+});
+
+function handleSubmitLogin() {
+  console.log("Login");
+}
+</script>
---
+++ app-chat/pages/register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-card shadow="always">
+      <el-form v-model="registerForm">
+        <el-form-item label="Login">
+          <el-input v-model="registerForm.login" />
+        </el-form-item>
+        <el-form-item label="Email">
+          <el-input v-model="registerForm.email" type="e-mail" />
+        </el-form-item>
+        <el-form-item label="Password">
+          <el-input v-model="registerForm.password" type="password" />
+        </el-form-item>
+        <el-form-item label="Repeat password">
+          <el-input v-model="registerForm.repeatPassword" type="password" />
+        </el-form-item>
+        <el-button type="primary" @click.prevent="handleSubmitRegister">
+          Login
+        </el-button>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const registerForm = ref({
+  login: "",
+  email: "",
+  password: "",
+  repeatPassword: "",
+});
+
+function handleSubmitRegister() {
+  console.log("Register");
+}
+</script>
