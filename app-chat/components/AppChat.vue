547edc4b26ef4361aaee869338d950f657cedaf6
Kacper Nalepa <kacper.nlp@wp.pl>
2024-02-15 20:22:15 +0100

    Add basic styles and structure for AppChat component

--- app-chat/components/AppChat.vue
+++ app-chat/components/AppChat.vue
@@ -1,10 +1,17 @@
 <template>
   <section>
-    <h2>Chat</h2>
-    <div>
-      <div v-for="({ username, message }, key) in messages" :key="key">
-        {{ username }}
-        {{ message }}
+    <h2 class="text-center mt-4">Chat</h2>
+    <div class="h-[60dvh] md:h-[60vh] bg-gray-100 mt-8 p-4 rounded-t-xl">
+      <div
+        v-for="({ username, message, userId }, key) in messages"
+        :key="key"
+        :class="{ 'is-logged-user': isLoggedUser(userId) }"
+        class="my-4"
+      >
+        <div class="message inline-block rounded-md bg-slate-200 p-2 text-sm">
+          <span v-if="!isLoggedUser(userId)">{{ username }}: </span>
+          <span>{{ message }}</span>
+        </div>
       </div>
     </div>
   </section>
@@ -17,5 +24,21 @@ interface Props {
   messages: ChatMessage[];
 }
 
+const store = useWebsiteStore();
+
 defineProps<Props>();
+
+function isLoggedUser(messageUserId: string) {
+  return messageUserId === store.loggedUser.id;
+}
 </script>
+
+<style lang="scss" scoped>
+.is-logged-user {
+  text-align: right;
+
+  .message {
+    background: #ddd;
+  }
+}
+</style>
